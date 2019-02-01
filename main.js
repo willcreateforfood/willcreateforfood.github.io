bd4e8e0969c3cb38c1ac64b9d631195aafb91417
willcreateforfood <38359673+willcreateforfood@users.noreply.github.com>
2019-01-31 21:43:41 -0800

    initial upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// window.onbeforeunload = function () {
+//   window.scrollTo(0, 0);
+// }
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// WORK PAGE JAVA
